a21c41616f856848676f03be90e2d5c2949b0602
Anas Bin Musa <www.showman00@gmail.com>
2026-08-04 13:16:11 +0000

    Fix missing comma

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def webhook():
 
                     try:
                         response = client.models.generate_content(
-                            model="gemini-flash-latest"
+                            model="gemini-flash-latest",
                             contents=user_text
                         )
 
